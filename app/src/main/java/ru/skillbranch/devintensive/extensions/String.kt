d22dda259505f0b9a00b7d7abfdd82b4fd05c366
psylon <stanislav.antunovich@yandex.ru>
2019-07-03 22:18:02 +0300

    *Необходимо реализовать метод truncate усекающий исходную строку до указанного числа символов и добавляющий заполнитель "..." в конец строки
    *Необходимо реализовать метод stripHtml для очистки строки от лишних пробелов, html тегов, escape последовательностей

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -0,0 +1,12 @@
+package ru.skillbranch.devintensive.extensions
+
+fun String.truncate(length: Int = 16): String =
+    if (this.trimEnd().length >= length)
+        "${this.substring(0, length).trimEnd()}..."
+    else
+        this.trimEnd()
+
+fun String.stripHtml(): String {
+    return replace(Regex("<.*?>"), "")
+        .replace(Regex("[\\s]{2,}"), " ")
+}
