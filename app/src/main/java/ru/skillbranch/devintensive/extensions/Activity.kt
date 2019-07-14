067764192ddc94674ca66e542d73391b3589a63b
psylon <stanislav.antunovich@yandex.ru>
2019-07-14 18:06:55 +0300

    Activity.hideKeyboard() added

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -1,2 +1,10 @@
 package ru.skillbranch.devintensive.extensions
 
+import android.app.Activity
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+
+fun Activity.hideKeyboard() {
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+}
